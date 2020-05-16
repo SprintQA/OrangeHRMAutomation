c9d8e97b0a28c883926c7ec3482e76ca3aa89ca5
SprintQA <support@sprintqa.com>
2020-05-16 10:23:37 +0530

    initial commit

---
+++ src/test/java/com/sprintqa/runner/Runner.java
@@ -0,0 +1,17 @@
+package com.sprintqa.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+					features = "src\\test\\resources\\feature\\Demo.feature", 
+					glue = "com.sprintqa.stepDef", 
+					monochrome = true,
+					plugin = {"json:target/cucumber.json", "junit:target/cucumber.xml"}
+				)
+public class Runner {
+
+}
